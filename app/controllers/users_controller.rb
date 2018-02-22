7fff26ad9a04d161f11d844c0e842751549b664e
ZacChen11 <zacchenshuai@gmail.com>
2018-02-22 17:48:28 -0500

    adding csv

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -43,6 +43,10 @@ class UsersController < ApplicationController
   def index
     @users = User.all
     @user = User.find(current_user.id)
+    respond_to do |format|
+      format.html
+      format.csv { send_data @users.to_csv, filename: "users-#{Date.today}.csv" }
+    end
   end
 
   def show
--- app/models/user.rb
+++ app/models/user.rb
@@ -1,4 +1,5 @@
 class User < ActiveRecord::Base
+  require 'csv'
   has_many :projects
   has_many :hours
   has_many :role_maps
@@ -9,4 +10,17 @@ class User < ActiveRecord::Base
       uniqueness: {case_sensitive: false}
   validates :password,  presence: true, length: {minimum: 4}
   has_secure_password
+
+  def self.to_csv
+    attributes = %w{user_name email first_name last_name}
+
+    CSV.generate(headers: true) do |csv|
+      csv << attributes
+
+      all.each do |user|
+        csv << attributes.map{ |attr| user.send(attr) }
+      end
+    end
+  end
+
 end
