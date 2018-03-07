b8bf56dfb63099f3e6ba49552c5ca239e68d8f07
ZacChen11 <zacchenshuai@gmail.com>
2018-03-07 17:32:58 -0500

    rename project

--- config/initializers/session_store.rb
+++ config/initializers/session_store.rb
@@ -1,3 +1,3 @@
 # Be sure to restart your server when you modify this file.
 
-Rails.application.config.session_store :cookie_store, key: '_work_system_session'
+Rails.application.config.session_store :cookie_store, key: '_GroupIt_session'
