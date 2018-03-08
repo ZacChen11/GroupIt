553224255c936b0e3a5f040a524d700e95e37586
ZacChen11 <zacchenshuai@gmail.com>
2018-03-08 14:13:38 -0500

    change report model attributes name, modify search method

--- app/controllers/reports_controller.rb
+++ app/controllers/reports_controller.rb
@@ -1,16 +1,15 @@
 class ReportsController < ApplicationController
 
-
+  before_action :is_reporter_before_action
 
   def generate
     @report = Report.new
-    if params[:tasks].present?
-      records = export_task(params[:tasks])
+    if params[:task_ids].present?
+      records = Task.where(id: params[:task_ids])
     end
-    if params[:users].present?
-      records = export_user(params[:users])
+    if params[:user_ids].present?
+      records = User.where(id: params[:user_ids])
     end
-
     respond_to do |format|
       format.html
       format.csv { send_data records.to_csv, filename: "report-#{Date.today}.csv" }
@@ -20,34 +19,23 @@ class ReportsController < ApplicationController
   def search
     @report = Report.new(params[:report])
     # check input starting time and ending time
-    if @report.start_time.present? && @report.end_time.blank?
-      # no ending time
-      flash.now.notice = "please choose ending time"
-      return render 'generate'
-    end
-    if @report.start_time.blank? && @report.end_time.present?
+    if @report.start_time.blank?
       # no starting time
       flash.now.notice = "please choose starting time"
       return render 'generate'
     end
-    if @report.start_time.present? && @report.end_time.present? && @report.start_time >= @report.end_time
+    if @report.end_time.blank?
+      # no ending time
+      flash.now.notice = "please choose ending time"
+      return render 'generate'
+    end
+    if @report.start_time >= @report.end_time
       # starting time >= ending time
       flash.now.notice = "ending time should be larger or equal to starting time, please choose again"
       return render 'generate'
     end
-    if @report.start_time.blank? && @report.end_time.blank?
-      # no input time
-      flash.now.notice = "please choose time"
-      return render 'generate'
-    end
 
-    # check task and user
-    if @report.task_checked.present? &&@report.user_checked.present?
-      # when choose user/task
-      flash.now.notice = "can't choose task and user at the same time, choose again"
-      return render 'generate'
-    end
-    if @report.task_checked.present?
+    if @report.task_or_user_checked == "task"
       if @report.task_status.present?
         # when choose task/task status
         @tasks = Task.created_between(@report.start_time, @report.end_time).task_status(@report.task_status)
@@ -57,43 +45,36 @@ class ReportsController < ApplicationController
       end
       return render 'generate'
     end
-    if@report.user_checked.present?
-      if @report.user_status.present?
+
+    if @report.task_or_user_checked == "user"
+      if @report.user_activated.present?
         # when choose user/user status
-        @report.user_status = string_to_boolean(@report.user_status)
-        @users = User.created_between(@report.start_time, @report.end_time).user_status(@report.user_status)
+        @report.user_activated = string_to_boolean(@report.user_activated)
+        @users = User.created_between(@report.start_time, @report.end_time).user_activated(@report.user_activated)
       else
         # when choose user
         @users = User.created_between(@report.start_time, @report.end_time)
       end
       return render 'generate'
     end
-    if @report.task_checked.blank? && @report.user_checked.blank?
+
+    if @report.task_or_user_checked.blank?
       flash.now.notice = "choose task or user"
       return render 'generate'
+
     end
   end
 
   private
-  def export_task(a)
-    records = Task.where(id: a.each{ |t| t})
-    return records
-  end
-
-  def export_user(a)
-    records = User.where(id: a.each{ |t| t})
-    return records
-  end
-
-  def is_reporter
+  def is_reporter_before_action
     if !current_user.has_role?("report manager")
       flash.notice = "You don't have the previlege"
       redirect_to current_user
     end
   end
 
-  def string_to_boolean(s)
-    if s == "true"
+  def string_to_boolean(string)
+    if string == "true"
       return true
     else
       return false
--- app/models/report.rb
+++ app/models/report.rb
@@ -1,4 +1,4 @@
 class Report
   include ActiveModel::Model
-  attr_accessor :start_time, :end_time, :task_checked, :task_status, :user_checked, :user_status
+  attr_accessor :start_time, :end_time, :task_or_user_checked, :task_status, :user_activated
 end
