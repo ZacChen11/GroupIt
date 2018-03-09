475e556429c6c5888782658ba02855cf9ffffe6d
ZacChen11 <zacchenshuai@gmail.com>
2018-03-09 11:29:22 -0500

    optimize load records method

--- app/controllers/reports_controller.rb
+++ app/controllers/reports_controller.rb
@@ -4,30 +4,16 @@ class ReportsController < ApplicationController
 
   def generate
     @report = Report.new
-    # if params[:task_ids].present?
-    #   records = Task.where(id: params[:task_ids])
-    # end
-    # if params[:user_ids].present?
-    #   records = User.where(id: params[:user_ids])
-    # end
     respond_to do |format|
       format.html
       format.csv {
+        @report = Report.new(params[:report])
         #search records again based on the params
-        if params[:report][:task_or_user_checked] == "task"
-          records = Task.created_between(params[:report][:start_time], params[:report][:end_time])
-          if params[:report][:task_status].present?
-            # when choose task and task status
-            records = records.task_status(params[:report][:task_status])
-          end
-        end
-        if params[:report][:task_or_user_checked] == "user"
-          records = User.created_between(params[:report][:start_time], params[:report][:end_time])
-          if params[:report][:user_activated].present?
-            # when choose user and user status
-            params[:report][:user_activated] = string_to_boolean(params[:report][:user_activated])
-            records = records.user_activated(params[:report][:user_activated])
-          end
+        load_records_from_search
+        if @tasks
+          records = @tasks
+        else
+          records = @users
         end
         #write out records into a csv file
         send_data records.to_csv, filename: "report-#{Date.today}.csv" }
@@ -39,40 +25,10 @@ class ReportsController < ApplicationController
     if @report.invalid?
       return render 'generate'
     end
-    # # validate input starting time and ending time
-    # if @report.start_time.blank?
-    #   # no starting time
-    #   flash.now.notice = "please choose start time"
-    #   return render 'generate'
-    # elsif @report.end_time.blank?
-    #   # no ending time
-    #   flash.now.notice = "please choose end time"
-    #   return render 'generate'
-    # elsif @report.start_time >= @report.end_time
-    #   # starting time >= ending time
-    #   flash.now.notice = "ending time should be larger or equal to starting time, please choose again"
-    #   return render 'generate'
-    # end
-
-    # load records based on search condition
-    if @report.task_or_user_checked == "task"
-      @tasks = Task.created_between(@report.start_time, @report.end_time)
-      if @report.task_status.present?
-        # when choose task and task status
-        @tasks = @tasks.task_status(@report.task_status)
-      end
-    end
-    if @report.task_or_user_checked == "user"
-      @users = User.created_between(@report.start_time, @report.end_time)
-      if @report.user_activated.present?
-        # when choose user and user status
-        @report.user_activated = string_to_boolean(@report.user_activated)
-        @users = @users.user_activated(@report.user_activated)
-      end
-    end
     if @report.task_or_user_checked.blank?
       flash.now.notice = "choose task or user"
     end
+    load_records_from_search
     return render 'generate'
   end
 
@@ -89,6 +45,23 @@ class ReportsController < ApplicationController
       return true
     else
       return false
+    end
+  end
+
+  def load_records_from_search
+    if @report.task_or_user_checked == "task"
+      @tasks = Task.created_between(@report.start_time, @report.end_time)
+      if @report.task_status.present?
+        # when choose task and task status
+        @tasks = @tasks.task_status(@report.task_status)
+      end
+    elsif @report.task_or_user_checked == "user"
+      @users = User.created_between(@report.start_time, @report.end_time)
+      if @report.user_activated.present?
+        # when choose user and user status
+        @report.user_activated = string_to_boolean(@report.user_activated)
+        @users = @users.user_activated(@report.user_activated)
+      end
     end
   end
 
